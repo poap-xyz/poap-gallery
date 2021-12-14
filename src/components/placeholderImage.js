7c4bd68265fbcb5ae950d965b2d5a41b23b5558f
Guido Alpern <guidoalpern@gmail.com>
2021-12-14 11:42:24 -0300

    Fix: Removed unnecesary placeholderImage component

--- src/components/placeholderImage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ReactComponent as Placeholder } from "../assets/images/placeholder.svg";
-
-export const PlaceHolderImage = ({ width = 80, height = 80 }) => (
-  <Placeholder width={width} height={height} />
-)
