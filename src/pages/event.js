c5946340626d81b9726c5a18283bce6165d1c280
rlajous <rodrigo@lajous.com.ar>
2023-08-16 17:31:25 +0200

    Align left text

--- src/pages/event.js
+++ src/pages/event.js
@@ -424,7 +424,7 @@ const ExternalLinkCell = ({ url, tooltipText = null, content }) => {
       data-cooltipz-dir="top"
       style={{
         position: 'relative',
-        maxWidth: 165,
+        maxWidth: 160,
         width: '100%',
       }}
       onClick={handleIconClick}
@@ -433,11 +433,11 @@ const ExternalLinkCell = ({ url, tooltipText = null, content }) => {
       <p
         style={{
           position: 'relative',
-          maxWidth: 165,
+          maxWidth: 160,
           width: '100%',
         }}
       >
-        {shrinkAddress(content, width > 768 ? 20 : 10)}
+        {shrinkAddress(content, width > 768 ? 18 : 10)}
       </p>
     </a>
   );
@@ -597,7 +597,7 @@ function TableContainer({ tokens, ensNames, pageCount: pc, loading }) {
                   rel="noopener noreferrer"
                   data-tip="View Collection in POAP.scan"
                   style={{
-                    maxWidth: 165,
+                    maxWidth: 160,
                     width: '100%',
                   }}
                 >
@@ -605,7 +605,7 @@ function TableContainer({ tokens, ensNames, pageCount: pc, loading }) {
                   <ReactTooltip effect="solid" />
                   <p
                     style={{
-                      maxWidth: 165,
+                      maxWidth: 160,
                       width: '100%',
                     }}
                   >
